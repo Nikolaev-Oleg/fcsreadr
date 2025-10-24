be3e5080ca4cd6cd33970a62e698f6ea7593f3d3
Nikolaev-Oleg <121944350+Nikolaev-Oleg@users.noreply.github.com>
2025-10-24 20:22:00 +0300

    Add files via upload

--- geom_pseudocolour.R
+++ geom_pseudocolour.R
@@ -3,13 +3,14 @@ GeomPseudocolour <- ggproto("GeomPseudocolour", Geom,
                             default_aes = aes(shape = 19, r = 0.1),
                             draw_key = draw_key_point,
                             
-                            draw_panel = function(data, panel_params, coord) {
+                            #grad = grad,
+                            
+                            draw_panel = function(data, panel_params, coord, grad = grad) {
                               coords <- coord$transform(data, panel_params)
                               colour = pseudocolour(coords$x, coords$y, coords$r)
                               
-                              grad <- colorRampPalette(std_gradient)(1000)
-                              coords$colour <- grad[as.numeric(cut(colour, breaks = 1000))]
-                              
+                              coords$colour <- grad[as.numeric(cut(colour, breaks = length(grad)))]
+                              coords<<-coords
                               grid::pointsGrob(
                                 coords$x, coords$y,
                                 pch = coords$shape,
@@ -20,10 +21,10 @@ GeomPseudocolour <- ggproto("GeomPseudocolour", Geom,
 
 geom_pseudocolour <- function(mapping = NULL, data = NULL, stat = "identity",
                               position = "identity", na.rm = FALSE, show.legend = NA, 
-                              inherit.aes = TRUE, ...) {
+                              inherit.aes = TRUE, grad = colorRampPalette(std_gradient)(1000), ...) {
   ggplot2::layer(
     geom = GeomPseudocolour, data = data, mapping = mapping, stat = stat, 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+    params = list(na.rm = na.rm, grad = grad, ...)
   )
 }
